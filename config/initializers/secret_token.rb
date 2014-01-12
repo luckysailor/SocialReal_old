f8d0ae47e4a66d589f13bcb57e779e6d07afeee0
saturnlove <saturn-unity@hushmail.com>
2014-01-11 23:05:18 -0200

    Application created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SocialReal::Application.config.secret_key_base = 'c3dd9fa89f8431f28a502260c984e298e2fc04064e51220ab5d176d552916d0cb69c731ce9996e445ae41053d3995f64f2a7b811fe62e2ab3477baabe0ce93ec'
