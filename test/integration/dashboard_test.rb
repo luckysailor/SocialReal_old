22aea1628389468d77dba17dd3e33a37fc0c5ed6
saturnlove <saturn-unity@hushmail.com>
2014-01-25 01:00:04 -0200

    Test name fixed

--- test/integration/dashboard_test.rb
+++ test/integration/dashboard_test.rb
@@ -8,7 +8,7 @@ class DashboardTest < ActionDispatch::IntegrationTest
  	assert_redirected_to dashboard_path
  end
 
- test "should deny access whether user is guest or major" do
+ test "should deny access whether user not are guest or major" do
  	user = create_user_with_bitmask 0
  	post_via_redirect '/users/sign_in',\
  	 user: { email: user.email, password: '12345678' }
